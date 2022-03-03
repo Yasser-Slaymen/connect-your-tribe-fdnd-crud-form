2d75c995f874d9a8b46405833c210de00192e903
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-03-03 23:59:13 +0100

    Update post.js

--- scripts/post.js
+++ scripts/post.js
@@ -1,39 +1,39 @@
-/* Define Elements */
-const createStudentForm = document.querySelector('#createStudentForm');
-const apiUrl = 'https://tribe.api.fdnd.nl/v1/member';
+// /* Define Elements */
+// const createStudentForm = document.querySelector('#createStudentForm');
+// const apiUrl = 'https://tribe.api.fdnd.nl/v1/member';
 
-// Create a submnit listener
-createStudentForm.addEventListener('submit', function (e) {
+// // Create a submnit listener
+// createStudentForm.addEventListener('submit', function (e) {
 
-    e.preventDefault();
+//     e.preventDefault();
     
 
 
-    // Create a data object with data from the create form.
-    let data = {
-        "squadId": parseInt(document.querySelector('#squadId').value),
-        "type": document.querySelector('#type').value,
-        "nickname": document.querySelector('#nickname').value,
-        "name": document.querySelector('#name').value,
-        "prefix": document.querySelector('#prefix').value,
-        "surname": document.querySelector('#surname').value,
-        "avatar": document.querySelector('#avatar').value,
-        "bio": document.querySelector('#bio').value,
-        "githubHandle": document.querySelector('#githubHandle').value,
-        "url": document.querySelector('#url').value,
-    }
+//     // Create a data object with data from the create form.
+//     let data = {
+//         "squadId": parseInt(document.querySelector('#squadId').value),
+//         "type": document.querySelector('#type').value,
+//         "nickname": document.querySelector('#nickname').value,
+//         "name": document.querySelector('#name').value,
+//         "prefix": document.querySelector('#prefix').value,
+//         "surname": document.querySelector('#surname').value,
+//         "avatar": document.querySelector('#avatar').value,
+//         "bio": document.querySelector('#bio').value,
+//         "githubHandle": document.querySelector('#githubHandle').value,
+//         "url": document.querySelector('#url').value,
+//     }
 
-    // Send the form data to the API
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then(function (res) {
-        return res.json;
-    }).catch(
-        error => console.log(error)
-    )
-})
+//     // Send the form data to the API
+//     fetch(apiUrl, {
+//         method: 'POST',
+//         headers: {
+//             Accept: "application/json",
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data),
+//     }).then(function (res) {
+//         return res.json;
+//     }).catch(
+//         error => console.log(error)
+//     )
+// })
