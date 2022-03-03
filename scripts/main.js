5d321fa26ddc41195637f76e317be0bef50fd366
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-03-03 13:15:16 +0100

    js updating

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const Base__Url = 'https://tribe.api.fdnd.nl/v1'
+const form = document.querySelector('#memberform')
 
 const renderMember = (data) => {
     data.data.forEach(post => {
@@ -21,6 +22,42 @@ fetch(Base__Url + '/member')
 
 
 // Nieuwe members toevoegen
+
+form?.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    console.log('halll')
+
+    // LET values
+    let member = {
+        memberId: ParseInt(memberForm.elements.memberId.value),
+            squadId: ParseInt(memberForm.elements.squadId.value),
+        type: memberForm.elements.type.value,
+        nickname: memberForm.elements.nickname.value,
+        name: memberForm.elements.name.value,
+        prefix: memberForm.elements.prefix.value,
+        surname: memberForm.elements.surname.value,
+            avatar: memberForm.elements.avatar.value,
+            githubHandle: memberForm.elements.githubHandle.value,
+        bio: memberForm.elements.bio.value,
+            url: memberForm.elements.url.value,
+      }
+
+    // fetching api
+    fetch(Base__Url, {
+        method: 'POST',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then(function (res) {
+        return res.json;
+    }).catch(
+        error => console.log(error)
+    )
+})
+
+
 
 
 
